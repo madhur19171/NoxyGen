aa9fa51c43e22ff3754cc1d86ce7b85333d4f35c
madhur19171 <madhur19171@iiitd.ac.in>
2021-10-30 19:33:44 +0530

    Designed Mesh NoC. Forwarding table yet to be generated.

---
+++ Router/Mesh33/Mesh33_modelsim.tcl
@@ -0,0 +1,4 @@
+vcom -2008 /home/dynamatic/Dynamatic/etc/dynamatic/components/elastic_components.vhd
+vcom -2008 /home/dynamatic/Dynamatic/etc/dynamatic/components/delay_buffer.vhd
+vcom -2008 /home/dynamatic/Dynamatic/etc/dynamatic/components/arithmetic_units.vhd
+vcom -2008 /home/dynamatic/Dynamatic/etc/dynamatic/components/MemCont.vhd
