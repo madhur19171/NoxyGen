43ee03d3438faa4db4f74eaf342f34c782d08519
divindominic265 <55682134+divindominic@users.noreply.github.com>
2021-12-07 01:25:08 +0530

    Port_extraction

--- vcd_standalone.cpp
+++ vcd_standalone.cpp
@@ -13,6 +13,25 @@ using std::hex;
 */
 int main(int argc, char **argv)
 {
+    int inp_state=0;
+    double inp_timestamp=0;
+    if(argc==2)
+    {
+        //output default print style.
+    }
+    else if (argc>2)
+    {
+        if(strcmp(argv[2],"-n")==0)
+        {
+            //output for dump file
+            inp_state=1;
+        }
+        else if(strcmp(argv[2],"-i")==0)
+        {
+            inp_state=2;
+            inp_timestamp=atof(argv[3]);
+        }
+    }
 
     std::string infile(argv[1]);
 
@@ -33,31 +52,23 @@ int main(int argc, char **argv)
         std::cout << "Times Recorded:" << trace->get_timestamps()->at(i) << std::endl;
         }
         std::cout << "hello";
-
         // Print out every signal in every scope.
         for (VCDScope *scope : *trace->get_scopes())
         {
-
             std::cout << "Scope: " << scope->name << std::endl;
-
             for (VCDSignal *signal : scope->signals)
             {
-
                 std::cout << "\t" << signal->hash << "\t"
                           << signal->reference;
                 std::cout<<trace->get_signal_value_at(signal->hash,trace->get_timestamps()->at(1),false);
-
                 if (signal->size > 1)
                 {
                     std::cout << " [" << signal->size << ":0]";
                 }
-
                 std::cout << std::endl;
             }
         }
-
         delete trace;
-
         return 0;
     }
     else
@@ -65,10 +76,12 @@ int main(int argc, char **argv)
         std::cout << "Parse Failed." << std::endl;
         return 1;
     }*/
-
+if(inp_state==0)
+{
     if(trace == nullptr) {
     // Something went wrong.
-} else {
+    } 
+    else {
 
     for(VCDScope * scope : *trace -> get_scopes()) {
 
@@ -89,18 +102,24 @@ int main(int argc, char **argv)
     }
 
 }
-
-for (VCDScope *scope : *trace->get_scopes())
+}
+else if(inp_state==1)
 {
-    std::cout << "Scope: " << scope->name << std::endl;
-    for (VCDSignal * mysignal : scope->signals){
-//VCDSignal * mysignal = trace -> get_scope("nodeVerifier0") -> signals[1];
-
-
-// Print the value of this signal at every time step.
-std::cout<<mysignal -> size;
-std::cout<<"size\n";
-for (VCDTime time : *trace -> get_timestamps()) {
+    for (VCDScope *scope : *trace->get_scopes())
+{
+    //std::cout << "Scope: " << scope->name << std::endl;
+    //char[256] s1="";
+    //strcpy(s1,scope->name);
+    std::string s1=scope->name;
+    std::string s2="port";
+    //std::cout<<s1;
+    if(s1.compare(s2)==0){
+        std::cout<<scope->name<<"\n";
+        for (VCDSignal * mysignal : scope->signals){
+            std::cout<<"\n";
+            std::cout << mysignal -> reference << " = ";
+        
+        for (VCDTime time : *trace -> get_timestamps()) {
 
     VCDValue * val = trace -> get_signal_value_at( mysignal -> hash, time);
 
@@ -155,7 +174,159 @@ for (VCDTime time : *trace -> get_timestamps()) {
     }
 
     std::cout << std::endl;
+        }
 
+    }
+    
+    //for (VCDSignal * mysignal : scope->signals){
+//VCDSignal * mysignal = trace -> get_scope("nodeVerifier0") -> signals[1];
+
+
+// Print the value of this signal at every time step.
+//std::cout<<"size "<<mysignal -> size<<"\n";
+
+
+    //VCDTime time=inp_timestamp;
+    //VCDValue * val = trace -> get_signal_value_at( mysignal -> hash, time);
+    //std::cout<<"\n";
+    //std::cout << mysignal -> reference
+              //<< " = ";
+    
+    // Assumes val is not nullptr!
+    
+    /*switch(val -> get_type()) {
+        case (VCD_SCALAR):
+        {
+            std::cout << VCDValue::VCDBit2Char(val -> get_value_bit());
+            break;
+        }
+        case (VCD_VECTOR):
+
+        {   std::string str3;
+            VCDBitVector * vecval = val -> get_value_vector();
+            for(auto it = vecval -> begin();
+                     it != vecval -> end();
+                     ++it) {
+                str3=str3+VCDValue::VCDBit2Char(*it);        
+                //std::cout << VCDValue::VCDBit2Char(*it);
+                
+
+            }
+            //std::cout <<str3;
+            
+            long int longint=0;
+                int len=str3.size();
+                for(int i=0;i<len;i++)
+                {
+                longint+=( str3[len-i-1]-48) * pow(2,i);
+
+
+                }
+                if(str3.length()==1){
+                    str3="00000000000000000000000000000000";
+                    std::cout<<str3;
+                }
+                else{
+                std::cout<<str3;}
+                //std::cout<<longint;}
+            break;
+    }
+        case (VCD_REAL):
+        {
+            std::cout << val -> get_value_real();
+        }
+        default:
+        {
+            break;
+        }
+    }
+    
+
+    std::cout << std::endl;
+*/
+}
 }}
+
+
+
+
+
+
+    //add your code...
+
+else if(inp_state==2)
+{
+
+for (VCDScope *scope : *trace->get_scopes())
+{
+    std::cout << "Scope: " << scope->name << std::endl;
+    for (VCDSignal * mysignal : scope->signals){
+//VCDSignal * mysignal = trace -> get_scope("nodeVerifier0") -> signals[1];
+
+
+// Print the value of this signal at every time step.
+std::cout<<"size "<<mysignal -> size<<"\n";
+
+
+    VCDTime time=inp_timestamp;
+    VCDValue * val = trace -> get_signal_value_at( mysignal -> hash, time);
+    
+    std::cout << mysignal -> reference
+              << " = ";
+    
+    // Assumes val is not nullptr!
+    
+    switch(val -> get_type()) {
+        case (VCD_SCALAR):
+        {
+            std::cout << VCDValue::VCDBit2Char(val -> get_value_bit());
+            break;
+        }
+        case (VCD_VECTOR):
+
+        {   std::string str3;
+            VCDBitVector * vecval = val -> get_value_vector();
+            for(auto it = vecval -> begin();
+                     it != vecval -> end();
+                     ++it) {
+                str3=str3+VCDValue::VCDBit2Char(*it);        
+                //std::cout << VCDValue::VCDBit2Char(*it);
+                
+
+            }
+            //std::cout <<str3;
+            
+            long int longint=0;
+                int len=str3.size();
+                for(int i=0;i<len;i++)
+                {
+                longint+=( str3[len-i-1]-48) * pow(2,i);
+
+
+                }
+                if(str3.length()==1){
+                    str3="00000000000000000000000000000000";
+                    std::cout<<str3;
+                }
+                else{
+                std::cout<<str3;}
+                //std::cout<<longint;}
+            break;
+    }
+        case (VCD_REAL):
+        {
+            std::cout << val -> get_value_real();
+        }
+        default:
+        {
+            break;
+        }
+    }
+    
+
+    std::cout << std::endl;
+
+}
+}
 }
 }
